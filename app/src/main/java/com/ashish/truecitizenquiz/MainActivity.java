f02a28cad00d3fb2a84a0e656ef1f21bee04bf48
Ashish Sharda <ashishjsharda@gmail.com>
2021-05-02 07:24:18 -0400

    Add Button and Listeners ,Define variables

--- app/src/main/java/com/ashish/truecitizenquiz/MainActivity.java
+++ app/src/main/java/com/ashish/truecitizenquiz/MainActivity.java
@@ -3,12 +3,31 @@ package com.ashish.truecitizenquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button falseButton;
+    private Button trueButton;
+    private TextView questionTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        falseButton=findViewById(R.id.false_button);
+        trueButton=findViewById(R.id.true_button);
+        questionTextView=findViewById(R.id.answer_text_view);
+        falseButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
